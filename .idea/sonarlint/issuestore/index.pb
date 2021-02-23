79b275a36527ffa7ddbd210d7cf96d16af37f60a
VU-Irakliy <i.marsagishvili@student.vu.nl>
2021-02-23 21:17:57 +0100

    TimerClass partial implementation and game.fxml change

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,6 +1,6 @@
 
 9
-	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
 7
 gradlew,5/b/5bbfa66edb4db3c7c33c5181f43510990d3307f9
 :
@@ -14,17 +14,23 @@ K
 .gradle/5.2.1/gc.properties,7/b/7b75c69449d10199b2abb505dc3dcb548478e60d
 K
 .gradle/vcs-1/gc.properties,3/f/3fa1ed8f1b4e1b9c233b6023870998c667d79f5c
-G
-.idea/libraries/lib.xml,5/6/563a23e064e6fdd2d09bd0c2d99ef456aff71a31
 I
 .idea/modules/Project.iml,f/a/fa375b78ad613ddb861b5dc53ffee5bf6fe09594
 R
-"src/main/java/game/Controller.java,8/6/86acadd96f76165491c60b7c82a0fc852ee6e82b
+"src/main/java/game/Controller.java,8\6\86acadd96f76165491c60b7c82a0fc852ee6e82b
 R
 "src/main/java/META-INF/MANIFEST.MF,6/3/63ef5783cbf71fdf39c1fd6ae6873fd812259930
 L
-src/main/java/game/Main.java,9/1/91cb5c9f2f351791f58d7bf21c8352e1ac9a7a7f
+src/main/java/game/Main.java,9\1\91cb5c9f2f351791f58d7bf21c8352e1ac9a7a7f
 L
-src/main/java/game/game.fxml,a/2/a20645800f5ef5b9ac2780248f66eb02d47dfb2f
-b
-2.gradle/buildOutputCleanup/buildOutputCleanup.lock,6/8/681afd083bbf367638ad19d5d46031fbbf83cee7
+src/main/java/game/game.fxml,a\2\a20645800f5ef5b9ac2780248f66eb02d47dfb2f
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+R
+"src/main/java/game/TimerClass.java,0\9\098ef289bef778118d1af4799da90af41a01ac9c
+W
+'.gradle/5.2.1/fileHashes/fileHashes.bin,6\a\6a9b14c174e67a875b3cba6a0294d95d15529308
+Z
+*.gradle/buildOutputCleanup/outputFiles.bin,a\f\affd37cd1229eda4e03578f6dd1caa731ade4dd4
+Y
+).gradle/5.2.1/javaCompile/taskHistory.bin,3\4\343be079470ba2d832f216820dfc708e1f530b72
--- src/main/java/game/TimerClass.java
+++ src/main/java/game/TimerClass.java
@@ -1,5 +1,76 @@
 package game;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
 public class TimerClass{
     int seconds = 60;
+    int signal;
+    int time;
+    // 0-success; 1-ran out of time; 2-failed complete; 3-quitted;
+    boolean timeStartStop = false;
+    boolean success = false;
+    boolean fail = false;
+    boolean ranOutOfTime = false;
+
+
+    //  NEED TO ADD IT TO THE SCENE, WHERE IT WOULD CHANGE the number each time
+    public void getTime(){
+        time = gameLaunched();
+        //return time;
+    } // this needs some changes probably
+
+    /*
+
+
+     */
+
+    private int gameLaunched(){
+        timeStartStop = true;
+        return gameTimer();
+    }
+
+    private int gameTimer(){
+        if(ranOutOfTime == true){
+            timeStartStop = false;
+            signal = 1;
+        }
+        // fail is true can happen only by another part of the game
+        if(fail == true){
+            timeStartStop = false;
+            signal = 2;
+        }
+        //same thing as fail code
+        if(success == true){
+            timeStartStop = false;
+            signal = 0;
+        }
+        if(timeStartStop = true){
+            if(seconds <= 0){
+                ranOutOfTime = true;
+                signal = 1;
+            }
+            else{
+                seconds--;
+                //print?
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                gameTimer();
+            }
+        }
+
+        return signal;
+    }
+
 }
