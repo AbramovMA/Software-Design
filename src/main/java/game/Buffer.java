86fde213bcec44865dd57cb0e4b122d1d0b75857
Fullfungo <66333930+Fullfungo@users.noreply.github.com>
2021-02-27 16:11:49 +0300

    Refactor Buffer

--- src/main/java/game/Buffer.java
+++ src/main/java/game/Buffer.java
@@ -1,35 +1,51 @@
 package game;
 
 final public class Buffer {
-    final private int max_size;
-    private int size;
-    final private String[] values;
+    final private int max_size;    // maximum amount of value the buffer can store
+    private int size;              // current amount of value the buffer can store
+    final private String[] values; // the values stored in the buffer
 
+    /**
+     * Constructor for a buffer with a predefined size `capacity`
+     **/
     public Buffer(int capacity){
         max_size = capacity;
         size = 0;
         values = new String[max_size];
     }
 
-    final public boolean add_value(String s){
+    /**
+     * Adds `new_value` to the end of the buffer
+     * Return value: 1 on success
+     *               0 on failure
+     **/
+    final public boolean add_value(String new_value){
         if (size < max_size) {
-            values[size] = s;
+            values[size] = new_value;
             size++;
             return true;
         }else
             return false;
     }
 
+    /**
+     * Returns 1 if the buffer reached its maximum capacity, 0 otherwise
+     **/
     final public boolean is_full(){
         return size == max_size;
     }
 
-    private boolean contains(String[] seq){
-        int length = seq.length;
+    /**
+     * Checks if a given `sequence` is contained in the buffer
+     * Return value: 1 on success
+     *               0 on failure
+     **/
+    private boolean contains(String[] sequence){
+        int length = sequence.length;
         for (int i = 0; i < size - length + 1; ++i){
             boolean found = true;
             for (int j = 0; j < length; ++j)
-                if (!values[i + j].equals(seq[j])) {
+                if (!values[i + j].equals(sequence[j])) {
                     found = false;
                     break;
                 }
@@ -40,10 +56,20 @@ final public class Buffer {
         return false;
     }
 
+    /**
+     * Checks if the goal sequence is contained in the buffer
+     * Return value: 1 on success
+     *               0 on failure
+     **/
     final public boolean contains_goal_sequence(){
-        String[] goal = new String[0]; // TO DO: change to goal_sequence.get_sequence()
+        String[] goal = new String[0]; // TODO: change to goal_sequence.get_sequence()
         return contains(goal);
     }
 
-    final public void show(){}
+    /**
+     * Draws the buffer on the screen
+     **/
+    final public void show(){
+        // TODO: implement me
+    }
 }
