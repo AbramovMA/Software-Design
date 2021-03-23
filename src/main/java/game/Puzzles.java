ccceeef22acc77f80e017262e1b1744266c5b2da
Fullfungo <66333930+Fullfungo@users.noreply.github.com>
2021-03-23 20:15:26 +0100

    Add random puzzle generation

--- src/main/java/game/Puzzles.java
+++ src/main/java/game/Puzzles.java
@@ -1,26 +1,38 @@
 package game;
+import java.util.Arrays;
+import java.util.Vector;
 import java.util.Random;
 
 public class Puzzles {
-    String[] pickedSequence = {};
-    String[][] pickedMatrix = {};
-    int buffSize = 0;
-    int sequenceSize = 0;
-    //int puzzleSize = 0;
-    Random randomPuzzle = new Random();
-    int randomNum = 0;
+    int matrix_size;
+    int buffSize;
+    int sequenceSize;
+    String[][] pickedMatrix;
+    String[] pickedSequence;
+
+    private Random rng;
+
+    private String[] value_set = {"1C", "55", "7A", "BD", "E9"};
 
 
     public void puzzleGenerator(){
-        //System.out.println("Pass 00");
+        rng = new Random();
 
-        randomNum = randomPuzzle.nextInt(5) + 1;
+        /*
+            //System.out.println("Pass 00");
 
-        //System.out.println("Pass 1");
-        puzzleGenerate(randomNum);
+        puzzleGenerate(rng.nextInt(5) + 1;);
+        */
+
+        matrix_size = 6; // may be changed to random
+        sequenceSize = 5 + new Random().nextInt(5);
+        buffSize = (int)(sequenceSize * 1.2) + 1;
+        pickedSequence = new String[sequenceSize];
+        generate_random_game(matrix_size, sequenceSize);
     }
 
     private void puzzleGenerate(int i){
+        matrix_size = 6;
         if(i == 1){
             System.out.println("i = 1");
             buffSize = 5;
@@ -121,8 +133,37 @@ public class Puzzles {
         }
     }
 
+    public void generate_random_game(int matrix_size, int sequence_size){
+        String[][] matrix_values = new String[matrix_size][matrix_size];
+        Random kek = new Random();
 
+        // generate matrix values
+        for (int i = 0; i < matrix_values.length; ++i)
+            for (int j = 0; j < matrix_values.length; ++j)
+                matrix_values[i][j] = value_set[kek.nextInt(value_set.length)];
 
+        // generate goal sequence
+        Vector<String> sequence_values = new Vector<String>();
+        java.util.Set<java.awt.Point> selected_cells = new java.util.HashSet();
+
+        int x = 0, y = 0;
+        boolean is_x = true;
+        while (sequence_values.size() < sequence_size){
+            if (is_x)
+                x = kek.nextInt(matrix_size);
+            else
+                y = kek.nextInt(matrix_size);
+            if (!selected_cells.contains(new java.awt.Point(x,y))) {
+                sequence_values.add(matrix_values[x][y]);
+                selected_cells.add(new java.awt.Point(x,y));
+                is_x = !is_x;
+            }
+        }
+
+        // set-up
+        pickedMatrix = matrix_values;
+        sequence_values.toArray(pickedSequence);
+    }
 
 
 }
